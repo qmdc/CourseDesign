c9e7ffc5cc91f3ca35c722d51abfc778d82aa562
千岛 <notre1024@163.com>
2022-07-07 09:30:05 +0800

    Excel&加载效果

--- src/main/java/com/qiandao/coursedesign/controller/RecordController.java
+++ src/main/java/com/qiandao/coursedesign/controller/RecordController.java
@@ -12,6 +12,11 @@ import com.qiandao.coursedesign.utils.SimpleDate;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +24,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -118,6 +129,84 @@ public class RecordController {
         session.setAttribute("loginsuccess",user);
 
         return "forward:/shop/recording";
+    }
+
+    @ApiOperation("导出商品销售记录表")
+    @RequestMapping("/shop/export")
+    @ResponseBody
+    public String export() {
+        List<Record> records = recordService.getBaseMapper().selectList(null);
+
+        String path = "/Users/ppsn/Desktop/";
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("截止到"+SimpleDate.nowDay());
+
+        Row row0 = sheet.createRow(0);
+        Cell cell00 = row0.createCell(0);
+        Cell cell01 = row0.createCell(1);
+        Cell cell02 = row0.createCell(2);
+        Cell cell03 = row0.createCell(3);
+        Cell cell04 = row0.createCell(4);
+        Cell cell05 = row0.createCell(5);
+        Cell cell06 = row0.createCell(6);
+        Cell cell07 = row0.createCell(7);
+        Cell cell08 = row0.createCell(8);
+        Cell cell09 = row0.createCell(9);
+        cell00.setCellValue("销售编号");
+        cell01.setCellValue("商品id");
+        cell02.setCellValue("用户id");
+        cell03.setCellValue("销售数量");
+        cell04.setCellValue("销售时间");
+        cell05.setCellValue("商品价格");
+        cell06.setCellValue("实付款");
+        cell07.setCellValue("使用积分");
+        cell08.setCellValue("商品描述");
+        cell09.setCellValue("商品图片");
+
+        int i = 1;
+        for (Record record : records) {
+            Row row = sheet.createRow(i);
+            Cell cell0 = row.createCell(0);
+            Cell cell1 = row.createCell(1);
+            Cell cell2 = row.createCell(2);
+            Cell cell3 = row.createCell(3);
+            Cell cell4 = row.createCell(4);
+            Cell cell5 = row.createCell(5);
+            Cell cell6 = row.createCell(6);
+            Cell cell7 = row.createCell(7);
+            Cell cell8 = row.createCell(8);
+            Cell cell9 = row.createCell(9);
+
+            cell0.setCellValue(record.getRecordId());
+            cell1.setCellValue(record.getItemId().toString());
+            cell2.setCellValue(record.getUserId());
+            cell3.setCellValue(record.getRecordNums());
+            cell4.setCellValue(record.getRecordTime());
+            cell5.setCellValue(record.getItemprice().toString());
+            cell6.setCellValue(record.getRecordMoney().toString());
+            cell7.setCellValue(record.getUseBranch());
+            cell8.setCellValue(record.getItemDes());
+            cell9.setCellValue(record.getItemImg());
+            i++;
+        }
+
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(path + "销售记录表.xlsx");
+            workbook.write(fileOutputStream);
+        } catch (IOException e) {
+            log.info("导出Excel失败～");
+            return "no";
+        } finally {
+            try {
+                assert fileOutputStream != null;
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return "ok";
     }
 
 }
